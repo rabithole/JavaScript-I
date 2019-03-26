ec24c1c7b8b4bbd5da822a6cd11fef847c86a52f
Robert Kulp <pedalinon@gmail.com>
2019-03-25 18:36:56 -0700

    Object.js finished. Still playing with it though

--- assignments/objects.js
+++ assignments/objects.js
@@ -72,15 +72,30 @@ console.log(intern5.firstName);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 
-// console.log('Hello, my name is ' + intern2.firstName + '!');
-function speak(int) {
-	return 'Hello, my name is ' + int + '!';
-}
+// function speak(int) {
+// 	return 'Hello, my name is ' + int + '!';
+// }
+
+// const speak = (int) => {
+// 	return 'Hello, my name is ' + int + '!';
+// }
+
+const speak = (int) => 'Hello, my name is ' + int + '!';
 
 console.log(speak(intern2.firstName));
+console.log('Hello, my name is ' + intern2.firstName);
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+intern5.mult = function(a,b){
+	return a * b;
+}
+
+
+
+console.log(intern5.mult(4,4));
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
